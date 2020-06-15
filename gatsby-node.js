e5942c7496c031c57ade3bf4967c5998c0598085
joshuarule <joshua@joshuarule.com>
2020-06-15 11:26:48 -0400

    remove commented code

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,53 +33,6 @@ exports.createPages = async ({ graphql, actions }) => {
 			},
 		})
   })
-
-  // const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  // return graphql(
-  //   `
-  //     {
-  //       allMdx(
-  //         sort: { fields: [frontmatter___date], order: DESC }
-  //         limit: 1000
-  //       ) {
-  //         edges {
-  //           node {
-  //             fields {
-  //               slug
-  //             }
-  //             frontmatter {
-  //               title
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // ).then(result => {
-  //   if (result.errors) {
-  //     throw result.errors
-  //   }
-
-  //   // Create blog posts pages.
-  //   const posts = result.data.allMdx.edges
-
-  //   posts.forEach((post, index) => {
-  //     const previous = index === posts.length - 1 ? null : posts[index + 1].node
-  //     const next = index === 0 ? null : posts[index - 1].node
-
-  //     createPage({
-  //       path: `blog${post.node.fields.slug}`,
-  //       component: blogPost,
-  //       context: {
-  //         slug: post.node.fields.slug,
-  //         previous,
-  //         next,
-  //       },
-  //     })
-  //   })
-
-  //   return null
-  // })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
